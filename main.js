9e26eb3f71bba0a67d4ac03f352e2f8945bfd013
To Yui Kai <yuikai@nexmind.ai>
2024-03-20 10:00:01 +0800

    Completed Js functionalities.

--- main.js
+++ main.js
@@ -6,37 +6,102 @@ const app = new Vue({
     month: null,
     year: null,
 
+    eDay: '',
+    eMonth: '',
+    eYear: '',
+
     rDay: '--',
     rMonth: '--',
     rYear: '--',
 
-    states: [ true, true, true ],
-  },
-  computed: {
-    eDay() {
-      if ( !this.day ) return 'This field is required';
-      if ( this.day > 31 || this.day < 1 ) return 'Must be valid day';
-      return '';
-    },
-    eMonth() {
-      if ( !this.month ) return 'This field is required';
-      if ( this.month > 12 || this.month < 1 ) return 'Must be valid month';
-      return '';
-    },
-    eYear() {
-      if ( !this.year ) return 'This field is required';
-      if ( this.year > new Date().getFullYear ) return 'Must be in the past';
-      return '';
-    },
-  },
+    valid: [ true, true, true ],
 
+    errorMsgs: {
+      empty: 'This field is required',
+      day: 'Must be a valid day',
+      month: 'Must be a valid month',
+      year: 'Must be in the past',
+    }
+  },
+  
   methods: {
-    calculate() {
-      if ( !(this.eDay === '') ) this.states[0] = false;
-      if ( !(this.eMonth === '') ) this.states[1] = false; 
-      if ( !(this.eYear === '') ) this.states[2] = false;
+    setValid( id, valid ) {
+      this.valid[id] = valid;
+    },
+    validate() {
+      if ( !this.day ) {
+        this.eDay = this.errorMsgs.empty;
+      } else if ( this.day > 31 || this.day < 1 ) {
+        this.eDay = this.errorMsgs.day;
+      } else {
+        this.eDay = '';
+      }
 
+      if ( !this.month ) {
+        this.eMonth = this.errorMsgs.empty;
+      } else if ( this.month > 12 || this.month < 1 ) {
+        this.eMonth = this.errorMsgs.month;
+      } else {
+        this.eMonth = '';
+      }
+
+      if ( !this.year ) {
+        this.eYear = this.errorMsgs.empty;
+      } else if ( this.year > new Date().getFullYear() ) {
+        this.eYear = this.errorMsgs.year;
+      } else {
+        this.eYear = '';
+      }
+
+      var inputDate = new Date( this.year + '/' + this.month + '/' + this.day );
       
+      if ( isNaN(inputDate) ) {
+        if ( this.eDay === '' ) {
+          this.eDay = this.errorMsgs.day;
+        }
+        if ( this.eMonth === '' ) {
+          this.eMonth = this.errorMsgs.month;
+        }
+        if ( this.eYear === '' ) {
+          this.eYear = this.errorMsgs.year;
+        }
+      }
+
+      if ( inputDate > new Date() ) {
+        this.eYear = this.errorMsgs.Year;
+      }
+
+      if ( this.eDay === '' && this.eMonth === '' && this.eYear === '' ) {
+        this.setValid(0, true);
+        this.setValid(1, true);
+        this.setValid(2, true);
+        
+        return true;
+      } else {
+        if ( !(this.eDay === '') ) {
+          this.setValid(0, false);
+        }
+        if ( !(this.eMonth === '') ) {
+          this.setValid(1, false);
+        }
+        if ( !(this.eYear === '') ) {
+          this.setValid(2, false);
+        }
+        return false;
+      }
+    },
+    calculate() {
+      if ( this.validate() ) {
+        var age = new Date( new Date() - new Date( this.year + '/' + this.month + '/' + this.day ) );
+
+        this.rDay = age.getDate();
+        this.rMonth = age.getMonth();
+        this.rYear = age.getFullYear();
+      } else {
+        this.rDay = '--';
+        this.rMonth = '--';
+        this.rYear = '--';
+      }
     },
   },
 });
